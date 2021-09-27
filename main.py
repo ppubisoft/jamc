97d8efed57f332103aaa9dcfd429204792901e76
lyiriyah <37027375+lyiriyah@users.noreply.github.com>
2021-09-27 21:39:29 +0100

    idk what the fuck happened but this is the correct code

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import os
 import time
 import eyed3
 import datetime
+import curses
 
 cachedir = os.environ['HOME'] + "/.cache/jamp/"
 
@@ -23,7 +24,7 @@ userfile = sys.argv[1]
 
 _, extension = os.path.splitext(userfile)
 
-if extension not in [".mp3", ".flac"]:
+if extension not in [".flac", ".mp3"]:
     basefile = AudioSegment.from_file(userfile)
     basefile.export(cachefile, format="mp3")
 else:
@@ -38,10 +39,47 @@ except AttributeError:
 playback = Playback(cachefile)
 playback.play()
 
-if printattrs:
-    print(f"{tag.title} - {tag.artist}")
+def die(n, message="Exiting jamc..."):
+    curses.curs_set(1)
+    print(message)
+    exit(n)
 
-while playback.curr_pos != playback.duration:
-    sys.stdout.write(f"{datetime.timedelta(seconds=round(playback.curr_pos))}/{datetime.timedelta(seconds=round(playback.duration))}\r")
-    sys.stdout.flush()
-    time.sleep(1)
+
+def handle_keypress(c, playback):
+    if c == ' ':
+        playback.resume() if playback.paused else playback.pause()
+    elif c == 'h' or c == "KEY_LEFT":
+        playback.seek(playback.curr_pos - 5)
+#    elif c == ord('j'):
+#        playback.set_volume(playback.volume - 0.1)
+#    elif c == ord('k'):
+#        playback.set_volume(playback.volume + 0.1)
+    elif c == 'l' or c == "KEY_RIGHT":
+        playback.seek(playback.curr_pos + 5)
+    elif c == 'q':
+        die(0)
+
+def main(stdscr, playback):
+    # hide cursor
+    curses.curs_set(0)
+    # make getch() non-blocking
+    stdscr.nodelay(True)
+    # init colourscheme
+    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    global printattrs # i used global, so sue me
+    if printattrs:
+        stdscr.addstr(f"{tag.title} - {tag.artist}\n", curses.color_pair(1))
+    while playback.curr_pos != playback.duration:
+        stdscr.addstr(f"{datetime.timedelta(seconds=round(playback.curr_pos))}/{datetime.timedelta(seconds=round(playback.duration))}\r", curses.color_pair(1))
+        try:
+            c = stdscr.getkey()
+        except curses.error:
+            continue
+        handle_keypress(c, playback)
+        stdscr.refresh()
+        time.sleep(0.1)
+
+try:
+    curses.wrapper(main, playback)
+except KeyboardInterrupt:
+    die(0)
